3bf1f43a81f7d09664cc23ae7b1e9b0867658a09
Thomas T <huulbaek@gmail.com>
2022-08-09 22:26:51 +0200

    Background

--- app.vue
+++ app.vue
@@ -3,7 +3,10 @@
     <Header />
     <main class="-mt-32">
       <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-        <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
+        <div
+          class="rounded-lg px-5 py-6 shadow-lg sm:px-6"
+          style="background-image: url('/gradient.jpg'); background-size: cover"
+        >
           <div class="h-full">
             <NuxtPage />
           </div>
